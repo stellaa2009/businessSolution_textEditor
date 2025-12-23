ca30859aa3549732a3ca95db03a2e414a7e544f9
stellaa2009 <stellaa19@nycstudents.net>
2025-12-23 11:58:27 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -14,8 +14,6 @@ hightlight = Button(root, text="highlight", background='yellow')
 li = Button(root, text="list", background='lightGray')
 
 
-
-
 #textbox
 inputtxt = Text(root, height = 30)
 
@@ -23,7 +21,6 @@ voice.grid(row=1,column=0, )
 hightlight.grid(row=1,column=1)
 li.grid(row=1,column=2,pady=30)
 inputtxt.grid(row = 5, column =0,columnspan=3,padx=40)
-
 
 
 # Start the GUI event loop

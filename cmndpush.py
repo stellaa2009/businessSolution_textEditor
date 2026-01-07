46160e646eafb74afcb548d58bf0f74b7a2e26a6
stellaa2009 <stellaa19@nycstudents.net>
2026-01-07 11:06:52 -0500

    Update cmndpush.py

--- cmndpush.py
+++ cmndpush.py
@@ -4,18 +4,24 @@ from tkmacosx import Button
 
 # Create the main window
 root = Tk()
-root.title("command push")
+root.title("cmndpush")
 
 #Set size of window
 root.geometry("700x700")
 
-voice = Button(root, text="change me.", background='lightGray')
-txt =Text(root, height 5, width 52)
+def changeTitle():
+	root.L(T.get("1.0", END))
+	pass
+	
+T =Text(root, height=5, width=52)
+L = Label(root, text="Label" )
+B = Button(root, text="change me.", background='lightGray',command= changeTitle)
 
 
-voice.grid(row=1,column=1, )
-txt.grid(row=1,column=2)
 
+B.grid(row=1,column=1,)
+T.grid(row=1,column=2)
+L.grid(row=1,column=3)
 
 
 

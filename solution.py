3645bc012b1529645f7f3bfb11843f1bf1061b1e
stellaa2009 <stellaa19@nycstudents.net>
2026-01-27 11:58:16 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,9 +9,25 @@ root.title("solution")
 #Set size of window
 root.geometry("700x700")
 
+def writing():
+	txt = inputtxt.get("1.0",END)
+	with open("demofile.txt", "w") as f:
+  		f.write(txt)
+	pass
+
+def reading():
+	with open("demofile.txt","r") as f:
+		content = f.read()
+		print(content)
+
+
+
 voice = Button(root, text="voice", background='lightGray')
 hightlight = Button(root, text="highlight", background='yellow')
 li = Button(root, text="list", background='lightGray')
+writ = Button(root, text="write file", background='lightGray',command= writing)
+read = Button(root, text="read file", background='lightGray',command=reading)
+
 
 
 #textbox
@@ -21,7 +37,8 @@ voice.grid(row=1,column=0, )
 hightlight.grid(row=1,column=1)
 li.grid(row=1,column=2,pady=30)
 inputtxt.grid(row = 5, column =0,columnspan=3,padx=40)
-
+writ.grid(row=6,column=0)
+read.grid(row=6,column=2)
 
 # Start the GUI event loop
 root.mainloop()

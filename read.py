05b9013838885119daa68a2dedc00ef1fdd83d99
stellaa2009 <stellaa19@nycstudents.net>
2026-01-27 11:42:03 -0500

    Update read.py

--- read.py
+++ read.py
@@ -9,6 +9,26 @@ root.title("read")
 #Set size of window
 root.geometry("300x300")
 
-inputtxt = Text(root, height = 30)
-write = Button(root, text="write file", background='lightGray',command=)
-read = Button(root, text="read file", background='lightGray')
+
+
+def writing():
+	txt = inputtxt.get("1.0",END)
+	with open("demofile.txt", "w") as f:
+  		f.write(txt)
+	pass
+
+def reading():
+	with open("demofile.txt","r") as f:
+		content = f.read()
+		print(content)
+
+inputtxt = Text(root, height = 10,width=30)
+writ = Button(root, text="write file", background='lightGray',command= writing)
+read = Button(root, text="read file", background='lightGray',command=reading)
+
+
+inputtxt.grid(row=1,column=1)
+writ.grid(row=2,column=1)
+read.grid(row=3,column=1)
+
+root.mainloop()
